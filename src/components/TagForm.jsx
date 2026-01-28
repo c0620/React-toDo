4de0311225fae4010b542e1276991ebca57c3ee5
Cmg <e.shimanskua@gmail.com>
2026-01-28 23:35:48 +0300

    feat: TagForm (preliminary)

--- src/components/TagForm.jsx
+++ src/components/TagForm.jsx
@@ -1,3 +1,5 @@
+import { colors } from "../data";
+
 export function AddEditTag({
   tagId = null,
   color = null,
@@ -6,4 +8,46 @@ export function AddEditTag({
   tag = null,
   title = null,
   done = false,
-}) {}
+}) {
+  let colorPickers = colors.map((color) => (
+    <>
+      <style>
+        {`
+      .C${color.main.replace("#", "")}[type='radio']::after {
+        width: 15px;
+        height: 15px;
+        border-radius: 15px;
+        top: -2px;
+        left: -1px;
+        position: relative;
+        background-color: ${color.main};
+        content: '';
+        display: inline-block;
+        visibility: visible;
+        border: 2px solid white;
+      }
+
+      .C${color.main.replace("#", "")}[type='radio']:checked::after {
+        background-color: ${color.dark};
+      }
+    `}
+      </style>
+      <input
+        className={`C${color.main.replace("#", "")}`}
+        type="radio"
+        style={{ accentColor: color.main }}
+        value={[color.main, color.dark]}
+      />
+    </>
+  ));
+
+  return (
+    <div>
+      <form onSubmit={console.log("s_")}>
+        <label>Название цели</label>
+        {colorPickers}
+        <button type="submin">Добавить цель</button>
+      </form>
+    </div>
+  );
+}
