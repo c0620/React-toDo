ff0c3072939d8d9f8f36d9898e03c2a9c42d1201
Cmg <e.shimanskua@gmail.com>
2026-02-10 18:47:53 +0300

    chore: add fonts, icons, and reset styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "./styles/reset.css";
 import "./index.css";
 import { TaskManager } from "./components/TaskManager";
 import Dashboard from "./components/Dashboard/Dashboard.jsx";
